21c0d50b1ab44a35e4051bedaa49831a8c555c58
bensaltana hassan <bensaltana10@gmail.com>
2024-03-14 13:02:03 +0000

    Add AppDbContext , And initial files

--- RestaurantAppServer/Program.cs
+++ RestaurantAppServer/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantAppServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(op
+    => op.UseSqlServer(builder.Configuration.GetConnectionString("myCon"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
