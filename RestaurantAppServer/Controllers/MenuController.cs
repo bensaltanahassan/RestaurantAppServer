5709151c06fbb34198d1fb46b799b4fcb3813e12
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-03-18 00:06:03 +0100

    CRUD for MenuController

--- RestaurantAppServer/Controllers/MenuController.cs
+++ RestaurantAppServer/Controllers/MenuController.cs
@@ -3,8 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestaurantAppServer.Data;
 using RestaurantAppServer.Data.Models;
+using RestaurantAppServer.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using RestaurantAppServer.Interfaces;
+using RestaurantAppServer.Services;
+
 
 namespace RestaurantAppServer.Controllers
 {
@@ -12,19 +16,135 @@ namespace RestaurantAppServer.Controllers
     [ApiController]
     public class MenuController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly AppDbContext _db;
+        private readonly IImageService _imageService;
 
-        public MenuController(AppDbContext context)
+        public MenuController(AppDbContext db, IImageService imageService)
         {
-            _context = context;
+            _db = db;
+            _imageService = imageService;
         }
 
         [HttpGet]
         [Route("GetItems")]
         public async Task<ActionResult<IEnumerable<Product>>> GetItems()
         {
-            var products = await _context.Products.Include(p => p.Category).ToListAsync();
+            var products = await _db.Products.Include(p => p.Category).ToListAsync();
             return Ok(products);
         }
+
+        [HttpPost]
+        [Route("CreateItems")]
+        public async Task<IActionResult> CreateProduct([FromForm] ProductModel pm, IFormFile file)
+        {
+            try
+            {
+                var result = await _imageService.AddImageAsync(file);
+                if (result.Error != null)
+                    return BadRequest(new { status = false, message = "Image upload failed" });
+
+                ImageModel img = new()
+                {
+                    PublicId = result.PublicId,
+                    Url = result.SecureUrl.AbsoluteUri
+                };
+
+                Image image = new()
+                {
+                    PublicId = img.PublicId,
+                    Url = img.Url
+                };
+
+                await _db.Images.AddAsync(image);
+                await _db.SaveChangesAsync();
+
+                Product product = new()
+                {
+                    Name = pm.Name,
+                    NameAn = pm.NameAn,
+                    Description = pm.Description,
+                    DescriptionAn = pm.DescriptionAn,
+                    Price = pm.Price,
+                    Discount = pm.Discount,
+                    NbrOfSales = pm.NbrOfSales,
+                    IsAvailable = pm.IsAvailable,
+                    CategoryId = pm.CategoryId,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    ProductImages = new List<ProductImages>
+            {
+                new ProductImages
+                {
+                    ImageId = image.Id 
+                }
+            }
+                };
+
+                await _db.Products.AddAsync(product);
+                await _db.SaveChangesAsync();
+
+                return Ok(new { status = true, message = "Product created successfully" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { status = false, message = "Internal Server Error", error = e.Message });
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] ProductModel pm)
+        {
+            try
+            {
+                var product = await _db.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return BadRequest(new { status = false, message = "Product not found" });
+                }
+
+                product.Name = pm.Name;
+                product.NameAn = pm.NameAn;
+                product.Description = pm.Description;
+                product.DescriptionAn = pm.DescriptionAn;
+                product.Price = pm.Price;
+                product.Discount = pm.Discount;
+                product.NbrOfSales = pm.NbrOfSales;
+                product.IsAvailable = pm.IsAvailable;
+                product.CategoryId = pm.CategoryId;
+
+                _db.Products.Update(product);
+                await _db.SaveChangesAsync();
+
+                return Ok(new { status = true, message = "Product updated successfully" });
+            }
+            catch
+            {
+                return BadRequest(new { status = false, message = "Internal Server Error" });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            try
+            {
+                var product = await _db.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return BadRequest(new { status = false, message = "Product not found" });
+                }
+
+                _db.Products.Remove(product);
+                await _db.SaveChangesAsync();
+
+                return Ok(new { status = true, message = "Product deleted successfully" });
+            }
+            catch
+            {
+                return BadRequest(new { status = false, message = "Internal Server Error" });
+            }
+        }
+
     }
 }
--- RestaurantAppServer/Models/ProductModel.cs
+++ RestaurantAppServer/Models/ProductModel.cs
@@ -10,5 +10,6 @@
         public int Discount { get; set; }
         public bool IsAvailable { get; set; }
         public int CategoryId { get; set; }
+        public int NbrOfSales { get; set; }
     }
 }
