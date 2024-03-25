ae771d5f8fedcab3990828796903c24c434cb5bf
Anas Hasna <hasnaanas230@gmail.com>
2024-03-25 17:51:16 +0000

    finished user controller commit

---
+++ RestaurantAppServer/Models/UpdateUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAppServer.Models
+{
+    public class UpdateUserModel
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public ImageModel? Image { get; set; }
+    }
+}
