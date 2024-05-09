b3c51670e89ad67172f443701781c0ac765af449
Anas Hasna <hasnaanas230@gmail.com>
2024-05-09 12:08:40 +0100

    commit changes and bug fixes

--- RestaurantAppServer/Controllers/DelivriesController.cs
+++ RestaurantAppServer/Controllers/DelivriesController.cs
@@ -71,6 +71,7 @@ namespace RestaurantAppServer.Controllers
                         Email = deliveryMan.Email,
                         FullName = deliveryMan.FullName,
                         PhoneNumber = deliveryMan.PhoneNumber,
+                        Status = deliveryMan.Status,
                         OrdersOnShipping = ordersOnShipping,
                         OrdersDelivered = ordersDelivered,
                     });
--- RestaurantAppServer/Models/DeliveryMenResponse.cs
+++ RestaurantAppServer/Models/DeliveryMenResponse.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
+        public string Status { get; set; }
         public int OrdersOnShipping { get; set; }
         public int OrdersDelivered { get; set; }
 
